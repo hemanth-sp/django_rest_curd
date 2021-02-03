d6391e6f8aa68e3d5ce124dc2247eae09cfdb461
hemanth <harshahemanth007@gmail.com>
2021-02-03 13:19:13 +0530

    class-based-api-view

--- curd/urls.py
+++ curd/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('students', students_list_or_create, name="students_list_or_create"),
-    path('students/<int:pk>/', students_get_or_update, name="students_get_or_update"),
+    path('students', StudentCurd.as_view(), name="students_list_or_create"),
+    path('students/<int:pk>/', StudentCurd.as_view(), name="students_get_or_update"),
 ]
--- curd/views.py
+++ curd/views.py
@@ -1,35 +1,38 @@
+from rest_framework.views import APIView
 from curd.serializers import StudentSerializers
 from curd.models import Student
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 
 
-@api_view(['GET', 'POST'])
-def students_list_or_create(request):
-    if request.method == "GET":
-        students_qs = Student.objects.all()
-        student_serializers = StudentSerializers(students_qs, many=True)
-        return Response(student_serializers.data, status=status.HTTP_200_OK)
-    else:
+class StudentCurd(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, pk=None, format=None):
+        if pk:
+            student = get_object_or_404(Student, id=pk)
+            student_serializers = StudentSerializers(student)
+            return Response(student_serializers.data, status=status.HTTP_200_OK)
+        else:
+            students_qs = Student.objects.all()
+            student_serializers = StudentSerializers(students_qs, many=True)
+            return Response(student_serializers.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
         student_serializers = StudentSerializers(data=request.data)
         student_serializers.is_valid(raise_exception=True)
         student_serializers.save()
         return Response(student_serializers.data, status=status.HTTP_201_CREATED)
 
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def students_get_or_update(request, pk):
-    student = get_object_or_404(Student, id=pk)
-    if request.method == "GET":
-        student_serializers = StudentSerializers(student)
-        return Response(student_serializers.data, status=status.HTTP_200_OK)
-    if request.method == "PUT":
+    def put(self, request, pk=None, format=None):
+        student = get_object_or_404(Student, id=pk)
         student_serializers = StudentSerializers(instance=student, data=request.data)
         student_serializers.is_valid(raise_exception=True)
         student_serializers.save()
         return Response(student_serializers.data, status=status.HTTP_200_OK)
-    if request.method == "DELETE":
+
+    def delete(self,request, pk=None, format=None):
+        student = get_object_or_404(Student, id=pk)
         student.delete()
         return Response({'msg': 'done'}, status=status.HTTP_204_NO_CONTENT)
